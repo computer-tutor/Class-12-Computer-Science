c3c06769879b85c9b3cb6ad64ed8cf1a1ef734da
Ankitpahuja <ankit.pahuja@st.niituniversity.in>
2020-05-02 19:48:53 +0530

    Unit 1 until 1.8

---
+++ Lecture Videos/Chapter 1/BearBones-Python.py
@@ -0,0 +1,23 @@
+''' This program shows a program's component
+    Helps you understand bearbones of a program '''
+
+# Definitin of function SeeYouSoon() follows
+
+def SeeYouSoon():
+    print("You are an amazing programmer!, See you soon :) \n")
+
+# Main program-code follows now
+
+print("Welcome to the world of Programming!\n")
+
+youngerSibling = 21
+
+elderSibling = youngerSibling + 5
+
+if (elderSibling > youngerSibling):       #colon means it's a block
+    print("You're good at +,-.\n")
+else:
+    print("You're not good at +,-.\n")
+
+SeeYouSoon()   #Calling above defined function SeeYouSoon()
+
---
+++ Study Material/PRIP/PRIP 1/PRIP 1.1/1.py
@@ -0,0 +1,14 @@
+# This is a solution to 1st program in PRIP 1.1
+
+# A - 1st Part of the program
+
+name = input("Enter your name: ")
+
+# B - 2nd Part of the program
+
+age = int(input("Enter your age: "))
+
+# C - 3rd Part of the program
+
+print("Your name is ", name)
+print("Your age is ", age)
